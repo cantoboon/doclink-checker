5b9b2bed8a62fa6dd5f5f56da2e2da31228ebf78
Lewis Boon <lewis.a.boon@gmail.com>
2020-10-29 20:40:31 +0000

    Adding some more test data

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::error::Error;
 
 fn main() {
     let mut current_dir = env::current_dir().unwrap();
-    current_dir.push("docs");
+    current_dir.push("test_data");
 
     scan_dir(current_dir);
 }
@@ -38,7 +38,6 @@ fn read_file(path: PathBuf) -> Result<String, io::Error> {
             },
             None => panic!("This should never happen - if we get a match .get(1) should be fine"),
         }
-        println!("Match in {} - {}", path.to_str().unwrap(), str_match.get(1).unwrap().as_str());
     }
 
     Ok(contents)
@@ -71,47 +70,3 @@ fn test_url(url: &str) -> Result<(), UrlTestError> {
 
     Ok(())
 }
-
-// fn get_diff() {
-//     let repo = match Repository::open("/home/lewis/rust/simple") {
-//         Ok(repo) => repo,
-//         Err(e) => panic!("Failed to open repo: {}", e),
-//     };
-
-//     let head = match repo.head() {
-//         Ok(head) => head,
-//         Err(e) => panic!("Failed to get head: {}", e),
-//     };
-
-//     println!("Head ref is: {}", head.name().unwrap());
-
-//     println!("Head commit is: {}", head.peel_to_commit().unwrap().id());
-
-
-//     let diff = match repo.diff_tree_to_tree(Some(&head.peel_to_tree().unwrap()), None, None) {
-//         Ok(diff) => diff,
-//         Err(e) => panic!("Failed to create diff: {}", e),
-//     };
-
-//     diff.foreach(
-//         &mut |d, i| -> bool {
-//             println!("file_cb: {} - {}", d.old_file().path().unwrap().to_str().unwrap(), i);
-//             true
-//         },
-//         None,
-//         None,
-//         None
-//         // &mut |d, b| -> bool {
-//         //     println!("binary_cb: {} - {}", d, b)
-//         //     true
-//         // },
-//         // &mut | d, h | -> bool {
-//         //     println!("hunk_cb: {} -{}", d, h)
-//         //     true
-//         // },
-//         // &mut |d, l| -> bool {
-//         //     println!("line_cb: {} - {}", d, l)
-//         //     true
-//         // }
-//     ).unwrap();
-// }
